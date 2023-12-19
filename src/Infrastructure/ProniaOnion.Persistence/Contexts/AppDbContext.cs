82b29d8dfb4127ff31fc29dd98827304dd4b3af6
T x <emraslanovsupan290@gmail.com>
2023-12-19 19:35:49 +0400

    Task Comleted

---
+++ src/Infrastructure/ProniaOnion.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Persistence.Contexts
+{
+    internal class AppDbContext
+    {
+    }
+}
