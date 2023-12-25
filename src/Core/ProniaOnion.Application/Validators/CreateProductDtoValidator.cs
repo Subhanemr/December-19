a7a7ed7571bdd4cddd9dd73d0b16373707e8c0d0
T x <emraslanovsupan290@gmail.com>
2023-12-25 21:24:53 +0400

    Fix Validator message

--- src/Core/ProniaOnion.Application/Validators/CreateProductDtoValidator.cs
+++ src/Core/ProniaOnion.Application/Validators/CreateProductDtoValidator.cs
@@ -17,11 +17,11 @@ namespace ProniaOnion.Application.Validators
                 .MinimumLength(1).WithMessage("Name min characters is 1");
 
             RuleFor(x => x.sku)
-                .NotEmpty().WithMessage("Name is required")
+                .NotEmpty().WithMessage("SKU is required")
                 .MaximumLength(10).WithMessage("SKU max characters is 10");
 
             RuleFor(x => x.price)
-              .NotEmpty().WithMessage("Name is required")
+              .NotEmpty().WithMessage("Price is required")
               .LessThanOrEqualTo(999999.99m).WithMessage("Price must not be more than 999999.99")
               .GreaterThanOrEqualTo(10).WithMessage("Price must not be small be 10");
 
